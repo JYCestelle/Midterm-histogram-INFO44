53e61b34ac1445c1f46d28ce3fa77adadbc2f7b7
yichej <yichej@uw.edum>
2019-05-14 00:14:28 -0700

    finish midterm.

--- js/midterm-jyc.js
+++ js/midterm-jyc.js
@@ -8,8 +8,8 @@
   window.onload = function() {
     svgContainer = d3.select('body')
       .append('svg')
-      .attr('width', 1200)
-      .attr('height', 500);
+      .attr('width', 1300)
+      .attr('height', 600);
 
     // d3.csv is basically fetch but it can be be passed a csv file as a parameter
     d3.csv("data/Seasons.csv")
@@ -36,61 +36,65 @@
     let mapFunctions = drawAxes(axesLimits, season_year, avg_viewer, svgContainer);
   
     // plot data as points and add tooltip functionality
-    //plotData(mapFunctions);
+    plotData(mapFunctions);
 
     // draw title and axes labels
     makeLabels();
+
+    // draw the average line for this data visualization
+    plotLine(avg_viewer, mapFunctions);
   }
 
   // make title and axes labels
   function makeLabels() {
     svgContainer.append('text')
-      .attr('x', 100)
+      .attr('x', 500)
       .attr('y', 40)
       .style('font-size', '14pt')
-      .text("Countries by Life Expectancy and Fertility Rate");
+      .text("Average Viewership By Season");
 
     svgContainer.append('text')
-      .attr('x', 500)
-      .attr('y', 490)
+      .attr('x', 650)
+      .attr('y', 550)
       .attr('text-anchor', 'middle')
       .style('font-size', '10pt')
       .style('font-weight', 'Bold')
       .text('Season Years');
 
     svgContainer.append('text')
-      .attr('transform', 'translate(15, 300)rotate(-90)')
+      .attr('transform', 'translate(60, 380)rotate(-90)')
       .style('font-size', '10pt')
       .style('font-weight', 'Bold')
       .text('Avg. Viewer (in millions)');
     
-    
+
+    // make legend for this data visualization
     svgContainer.append("rect")
-      .attr("x", 600)
+      .attr("x", 1000)
       .attr("y", 35)
       .attr("width", 150)
-      .attr("height", 60)
-      .style("stroke", "black")
+      .attr("height", 80)
+      .style("stroke", "darkgray")
       .style("fill", "none")
       .style("stroke-width", 1);
 
     svgContainer.append("text")
-      .attr("x", 605)
-      .attr("y", 50)
+      .attr("x", 1020)
+      .attr("y", 60)
       .text("Viewership Data");
 
     svgContainer.append("rect")
-      .attr("x", 605)
-      .attr("y", 60)
+      .attr("x", 1020)
+      .attr("y", 70)
       .attr("width", 10)
       .attr("height", 10)
       .style("stroke", "black")
-      .style("fill", "blue")
+      .style("fill", "lightblue")
       .style("stroke-width", 1);
 
     svgContainer.append("rect")
-      .attr("x", 605)
-      .attr("y", 80)
+      .attr("x", 1020)
+      .attr("y", 90)
       .attr("width", 10)
       .attr("height", 10)
       .style("stroke", "black")
@@ -98,61 +102,113 @@
       .style("stroke-width", 1);
 
     svgContainer.append("text")
-      .attr("x", 620)
-      .attr("y", 70)
+      .attr("x", 1040)
+      .attr("y", 80)
       .text("Acutal");
 
     svgContainer.append("text")
-      .attr("x", 620)
-      .attr("y", 90)
+      .attr("x", 1040)
+      .attr("y", 100)
       .text("Estimated");
   }
 
   // plot all the data points on the SVG
   // and add tooltip functionality
   function plotData(map) {
-    // get population data as array
-    let pop_data = data.map((row) => +row["pop_mlns"]);
-    let pop_limits = d3.extent(pop_data);
-    // make size scaling function for population
-    let pop_map_func = d3.scaleLinear()
-      .domain([pop_limits[0], pop_limits[1]])
-      .range([3, 20]);
-
     // mapping functions
     let xMap = map.x;
     let yMap = map.y;
 
     // make tooltip
     let div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
+    let barColor = function (d) {
+      if (d["Data"] == "Actual") {
+        return "lightblue";
+      } else {
+        return "grey";
+      }
+    }
+    console.log(data);
 
-
-    // append data to SVG and plot as points
-    svgContainer.selectAll('.dot')
+    svgContainer.selectAll(".bar")
       .data(data)
       .enter()
-      .append('circle')
-        .attr('cx', xMap)
-        .attr('cy', yMap)
-        .attr('r', (d) => pop_map_func(d["pop_mlns"]))
-        .attr('fill', "#4286f4")
-        // add tooltip functionality to points
-        .on("mouseover", (d) => {
-          div.transition()
-            .duration(200)
-            .style("opacity", .7);
-          div.html(d.location + "<br/>" + numberWithCommas(d["pop_mlns"]*1000000))
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", (d) => {
-          div.transition()
-            .duration(500)
-            .style("opacity", 0);
-        });
+      .append("rect")
+      .attr("class", "bar")
+      .attr('x', function (d) {
+        return xMap(d) + 50;
+      })
+      .attr('y', function (d) {
+        return yMap(d);
+      })
+      .attr("width", map.xScale.bandwidth())
+      .attr("height", function (d) {
+        return 500 - yMap(d);
+      })
+      .style("fill", function (d) { return barColor(d) })
+      // add tooltip functionality to points
+      .on("mouseover", (d) => {
+        d3.select(this)
+        .transition()
+        .duration(300)
+        .attr('opacity', 0.6)
+
+        div.transition()
+          .duration(200)
+          .style("opacity", .9);
+        div.html("Season #" + d["Year"] +
+          "<br/>Year: " + d["Year"] +
+          "<br/>Episodes: " + d["Episodes"] +
+          "<br/>Avg. Viewers (mil): " + d["Avg. Viewers (mil)"] +
+          "<br/>Most watched episode: " + d["Most watched episode"] +
+          "<br/>Viewers (mil): " + d["Viewers (mil)"])
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 40) + "px")
+          .style("width", "200px")
+          .style("height", "130px");
+      })
+      .on("mouseout", (d) => {
+        div.transition()
+          .duration(500)
+          .style("opacity", 0);
+      });
+
+    svgContainer.selectAll(".text")
+      .data(data)
+      .enter().append("text")
+      .attr("class", "bar")
+      .attr("text-anchor", "middle")
+      .attr("x", function (d) { return xMap(d) + map.xScale.bandwidth() / 2 + 50; })
+      .attr("y", function (d) { return yMap(d) - 10; })
+      .text(function (d) { return d["Avg. Viewers (mil)"]; });
+  }
+
+  function plotLine(avgView, scale) {
+
+    let yScale = scale.yScale;
+    let ave = avgView.reduce((total, amount) => total + amount) / avgView.length;
+    ave = ave.toFixed(1);
+
+    svgContainer.selectAll('line.avg')
+      .data(ave)
+      .enter()
+      .append('line')
+      .attr('x1', 100)
+      .attr("y1", function (d) { return yScale(ave); })
+      .attr("x2", 1200)
+      .attr("y2", function (d) { return yScale(ave); })
+      .style("stroke-dasharray", ("4, 4"));
+
+    let yPosition = yScale(ave);
+
+    svgContainer.append("text")
+      .attr("x", 1000)
+      .attr("y", yPosition - 5)
+      .style('font-size', '12pt')
+      .text("Average Viewers: " + ave);
   }
 
   // draw the axes and ticks
@@ -161,10 +217,9 @@
     let xValue = function (d) { return d["Year"]; }
 
     // function to scale x value
-    
     let xScale = d3.scaleBand()
-      .range([50, 1000])
-      .padding(.4);
+      .range([50, 1200])
+      .padding(0.3);
     xScale.domain(x);
     console.log(x);
 1
@@ -174,11 +229,12 @@
     // plot x-axis at bottom of SVG
     let xAxis = d3.axisBottom().scale(xScale);
     svg.append("g")
-      .attr('transform', 'translate(0, 450)')
+      .attr('transform', 'translate(50, 500)')
       .call(xAxis);
 
     // return y value from a row of data
-    let yValue = function(d) { return +d[y]}
+    let yValue = function(d) { 
+      return +d["Avg. Viewers (mil)"]}
 
     // function to scale y
     let yScale = d3.scaleLinear()
@@ -191,7 +247,7 @@
     // plot y-axis at the left of SVG
     let yAxis = d3.axisLeft().scale(yScale);
     svg.append('g')
-      .attr('transform', 'translate(50, 0)')
+      .attr('transform', 'translate(100, 50)')
       .call(yAxis);
 
     // return mapping and scaling functions
@@ -222,10 +278,4 @@
       yMax : yMax
     }
   }
-
-  // format numbers
-  function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-
 })();
